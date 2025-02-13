c4e8ac8501af10fdbbf160879cfbc3b64f77ffd5
Haseef Muhammed <haseefmuhammedpc@gmail.com>
2025-02-13 12:01:46 +0530

    initial repo

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,29 @@ AOS.init({
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
 });
+
+// Counter js
+
+    // Initialize PureCounter
+    document.addEventListener("DOMContentLoaded", function () {
+      new PureCounter();
+    });
+
+
+  //   * Init swiper sliders
+  //  */
+  function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+
+  window.addEventListener("load", initSwiper);
